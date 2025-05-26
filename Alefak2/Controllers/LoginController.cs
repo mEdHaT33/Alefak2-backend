c5fce078a8c12e7bb461dd82ccc7692b1d2f7ec4
mEdHaT33 <20201701703@cis.asu.edu.eg>
2025-05-26 11:17:58 +0300

    Login Request

---
+++ Alefak2/Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using Alefak2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Alefak2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+
+        private readonly ApiContext _context;
+
+        public LoginController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("check")]
+        public async Task<IActionResult> CheckLogin([FromBody] LoginRequest request)
+        {
+            if (string.IsNullOrEmpty(request.UserName) || string.IsNullOrEmpty(request.Password))
+            {
+                return BadRequest("Username and password are required.");
+            }
+
+            var user = await _context.users
+                .FirstOrDefaultAsync(u => u.UserName == request.UserName && u.Password == request.Password);
+
+            if (user == null)
+            {
+                return Unauthorized("Invalid username or password.");
+            }
+
+            return Ok(new
+            {
+                message = "Login successful",
+                user.ID,
+                user.UserName
+                
+            });
+        }
+    }
+}
