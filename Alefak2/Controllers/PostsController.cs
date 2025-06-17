57f1825e86c43a3603dd0584a6d508a7e6cbbbe5
mEdHaT33 <20201701703@cis.asu.edu.eg>
2025-06-17 13:10:34 +0300

    .

--- Alefak2/Controllers/PostsController.cs
+++ Alefak2/Controllers/PostsController.cs
@@ -52,7 +52,7 @@ namespace Alefak2.Controllers
             }
 
             // Redirect to deep link the app can handle
-            return Redirect($"Alefak://25a7-81-10-3-167.ngrok-free.app/post/{id}");
+            return Redirect($"https://25a7-81-10-3-167.ngrok-free.app/post/{id}");
         }
 
         [HttpGet("auther/{AuthorID}")]
